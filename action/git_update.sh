1afa443eed0dcd37ce922ceb97ebe38547e8f65b
ZenLyf <shashank.rv33515@gmail.com>
2024-03-14 19:42:35 +0530

    12rd commit

--- action/git_update.sh
+++ action/git_update.sh
@@ -43,16 +43,6 @@ echo "($VERSION) updayeing $CURRENT_VERSION to $NEW_TAG"
 GIT_COMMIT=`git rev-parse HEAD`
 NEEDS_TAG=`git describe --contains $GIT_COMMIT 2>/dev/null`
 
-# only tag if no tag already
-if [-z "$NEEDS_TAG" ]; then
-  echo "Tagged with $NEW_TAG"
-  git tag $NEW_TAG
-  git push --tags
-  git push
-else
-  echo "Already a tag on this commit"
-fi
-
-echo ::set-output name=new-version::$NEW_Tag
+echo ::set-output name=new-version::$NEW_TAG
 
 exit 0
