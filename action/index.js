117f3c7921bedaf9d15084a2106395bd62c2515c
ZenLyf <shashank.rv33515@gmail.com>
2024-03-14 19:25:44 +0530

    8rd commit

--- action/index.js
+++ action/index.js
@@ -27844,8 +27844,7 @@ const exec = __nccwpck_require__(5486);
 async function run() {
     try {
         const versionType= core.getInput('version-type');
-        const src = process.cwd();
-        // rest of your code
+        const src = __dirname;
 
         await exec.exec(`${src}/git_update.sh -v ${versionType}`);
     } catch(error) {
--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const exec = require('@actions/exec');
 async function run() {
     try {
         const versionType= core.getInput('version-type');
-        const src = process.cwd();
-        // rest of your code
+        const src = __dirname;
 
         await exec.exec(`${src}/git_update.sh -v ${versionType}`);
     } catch(error) {
